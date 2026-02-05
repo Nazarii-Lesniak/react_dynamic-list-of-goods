6dff2379edd25bfd9fbea6c4358e7234cd8a6a4e
Nazarii Lesniak <naziksay@gmail.com>
2026-02-05 17:26:13 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [list, setList] = useState<Good[]>([]);
+  const [isError, setIsError] = useState(false);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleLoadAll = () => {
+    return getAll()
+      .then(loadedGoods => setList(loadedGoods))
+      .catch(() => setIsError(true));
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleLoadFive = () => {
+    return get5First()
+      .then(loadedGoods => setList(loadedGoods))
+      .catch(() => setIsError(true));
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleLoadRed = () => {
+    return getRedGoods()
+      .then(loadedGoods => setList(loadedGoods))
+      .catch(() => setIsError(true));
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" data-cy="all-button" onClick={handleLoadAll}>
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleLoadFive}
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" data-cy="red-button" onClick={handleLoadRed}>
+        Load red goods
+      </button>
+
+      <GoodsList goods={list} />
+      {isError && <p style={{ color: 'red' }}>Something went wrong!</p>}
+    </div>
+  );
+};
